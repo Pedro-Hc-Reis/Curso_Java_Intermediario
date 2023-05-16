2cfb19316db396e318039f31a975ac939d8b2494
Pedro-Hc-Reis <131812725+Pedro-Agro@users.noreply.github.com>
2023-05-16 15:58:28 -0300

    01

---
+++ aula65/InformacaoAula.java
@@ -0,0 +1,10 @@
+package aula65;
+
+@interface InformacaoAula {
+
+    String autor();
+    int aulaNumero();
+    String blog() default "http://xxxxx.bom.br";
+    String site() default "http://xxxxx.bom.br";
+
+}
